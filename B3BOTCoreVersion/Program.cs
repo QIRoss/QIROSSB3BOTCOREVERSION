70a2ed86b742e2e22262b33980bb4908812692d2
Lucas Queiroz <lucasqreis@gmail.com>
2021-01-09 02:58:16 -0300

    Core version

--- B3BOTCoreVersion/Program.cs
+++ B3BOTCoreVersion/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Configuration;
+using Microsoft.Extensions.Configuration;
 using System.Timers;
+using System.IO;
 
 namespace B3BOTCoreVersion
 {
     class Program
     {
         private static Timer _aTimer;
-
+        public static IConfigurationRoot Configuration { get; set; }
         public static int Main(string[] arg)
         {
             if (arg.Length != 3)
@@ -17,21 +18,26 @@ namespace B3BOTCoreVersion
                 );
                 return 1;
             }
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
 
-            var user = ConfigurationManager.AppSettings["username"];
-            var password = ConfigurationManager.AppSettings["password"];
-            var apiUrl = ConfigurationManager.AppSettings["apiurl"];
-            var host = ConfigurationManager.AppSettings["host"];
-            var from = ConfigurationManager.AppSettings["from"];
-            var to = ConfigurationManager.AppSettings["recipients"];
-            var subject = ConfigurationManager.AppSettings["subject"];
-            var defaultBody = ConfigurationManager.AppSettings["defaultBody"];
-            var onBuying = ConfigurationManager.AppSettings["bodyOnBuying"];
-            var onSelling = ConfigurationManager.AppSettings["bodyOnSelling"];
+            Configuration = builder.Build();
+            
+            var user = Configuration["username"];
+            var password = Configuration["password"];
+            var apiUrl = Configuration["apiurl"];
+            var host = Configuration["host"];
+            var from = Configuration["from"];
+            var to = Configuration["recipients"];
+            var subject = Configuration["subject"];
+            var defaultBody = Configuration["defaultBody"];
+            var onBuying = Configuration["bodyOnBuying"];
+            var onSelling = Configuration["bodyOnSelling"];
 
             var connection = new Api(arg[0], float.Parse(arg[1]), float.Parse(arg[2]), apiUrl);
             var mail = new Mail(host, user, password, from, to, subject, defaultBody + '\n' + '\n');
-
+            
             void OnTimedEvent(Object source, ElapsedEventArgs e)
             {
                 Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
@@ -51,7 +57,7 @@ namespace B3BOTCoreVersion
                 }
             }
 
-            _aTimer = new Timer {Interval = 300000};
+            _aTimer = new Timer {Interval = 30000};
             _aTimer.Elapsed += OnTimedEvent;
             _aTimer.AutoReset = true;
             _aTimer.Enabled = true;
